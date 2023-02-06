40c989ebde9369ec5049e1a1b04bf20633833425
miyuki-shiro <thaimyg@yahoo.cl>
2023-02-05 21:39:25 -0300

    :sparkles: Added actions file

---
+++ app/src/actions/publications.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'http://localhost:4000/api',
+  headers: { 'Content-type': 'application/json' },
+});
+
+export const createPublication = (name, description) => async (dispatch) => {
+  try {
+    const response = await http.post('/publication', { name, description });
+    if (response.status === 200 || response.status === 201) {
+      dispatch({ type: 'CREATE', payload: response.data });
+    } else {
+      dispatch({ type: 'CREATE_ERROR', payload: response.data });
+    }
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: 'CREATE_ERROR', payload: { message: 'Could not create a new publication' }});
+  }
+};
+
+export const listPublications = () => async (dispatch) => {
+  try {
+    const response = await http.get('/publications');
+    if (response.status === 200) {
+      dispatch({ type: 'LIST', payload: response.data });
+    } else {
+      dispatch({ type: 'LIST_ERROR', payload: response.data });
+    }
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: 'LIST_ERROR', payload: { message: 'Could not load publications' }});
+  }
+}
+
+export const deletePublication = (id) => async (dispatch) => {
+  try {
+    const response = await http.delete(`/publication/${id}`);
+    if (response.status === 200) {
+      dispatch({ type: 'DELETE', payload: { id } });
+    } else {
+      dispatch({ type: 'DELETE_ERROR', payload: response.data });
+    }
+  } catch (err) {
+    console.log(err);
+    dispatch({ type: 'LIST_ERROR', payload: { message: 'Could not delete publication selected' }});
+  }
+};
